53f56a97d4c673eff034a66dffe7abeb34d9fff6
Shawn <sgraven66@gmail.com>
2024-04-30 18:09:25 -0500

    Changed building order

--- build_data.R
+++ build_data.R
@@ -1,10 +1,12 @@
 rstudioapi::restartSession(clean = TRUE)
 
 # Install package
-rm(list = ls())
 devtools::clean_dll()
+rm(list = ls())
 devtools::document()
-devtools::install()
+devtools::install() # Or clean and install in Build
+# (Required if some data chaged, will cause data coruption issues on the compiled package)
+# Rcmd.exe INSTALL --preclean --no-multiarch --with-keep.source ZekDex
 
 gen_national(write = TRUE)
 
