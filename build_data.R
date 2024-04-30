088824681c1bd0f45b6cc6bdc41d4edec09a1e3b
Shawn <sgraven66@gmail.com>
2024-04-30 18:09:39 -0500

    Added groups and more info

--- build_data.R
+++ build_data.R
@@ -8,11 +8,18 @@ devtools::install() # Or clean and install in Build
 # (Required if some data chaged, will cause data coruption issues on the compiled package)
 # Rcmd.exe INSTALL --preclean --no-multiarch --with-keep.source ZekDex
 
+
+gen_groups(write = TRUE)
+
+# Requires PokemonGroups
 gen_national(write = TRUE)
 
 
+gen_stats(write = TRUE)
+# Requires PokemonGroups
 gen_evos(write = TRUE)
+
+# Requires PokemonNational
 gen_lang(write = TRUE)
 gen_reginal(write = TRUE)
-gen_stats(write = TRUE)
 gen_type(write = TRUE)
