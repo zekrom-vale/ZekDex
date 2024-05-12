c460051f4907d162f9c8f0fb4ad569cb284b9091
Shawn <sgraven66@gmail.com>
2024-05-12 18:08:54 -0500

    Changed it to generate md #5
    GitHub wiki expects md

--- packing/sidebar.R
+++ packing/sidebar.R
@@ -7,54 +7,56 @@ files <- list.files("wiki", pattern = ".md", recursive = TRUE)
 # Remove the .md extension from each file
 files <- str_remove(files, ".md")
 
+# Exclude the _Sidebar file
+files <- files[files != "_Sidebar"]
+
 # Split the file paths to get nested directories
 split_files <- str_split(files, "/")
 
 # Recursive function to build a nested list
 build_nested_list <- function(paths, lst) {
-	if (length(paths) == 0) {
-		return(lst)
-	}
+  if (length(paths) == 0) {
+    return(lst)
+  }
 
-	path <- paths[[1]]
-	paths <- paths[-1]
+  path <- paths[[1]]
+  paths <- paths[-1]
 
-	if (length(path) == 1) {
-		lst[[path[[1]]]] <- path[[1]]  # Use the file name to represent a file
-	} else {
-		if (!path[1] %in% names(lst)) {
-			lst[[path[1]]] <- list()
-		}
-		lst[[path[1]]] <- build_nested_list(list(path[-1]), lst[[path[1]]])
-	}
+  if (length(path) == 1) {
+    lst[[path[[1]]]] <- path[[1]]  # Use the file name to represent a file
+  } else {
+    if (!path[1] %in% names(lst)) {
+      lst[[path[1]]] <- list()
+    }
+    lst[[path[1]]] <- build_nested_list(list(path[-1]), lst[[path[1]]])
+  }
 
-	return(build_nested_list(paths, lst))
+  return(build_nested_list(paths, lst))
 }
 
 # Build the nested list
 nested_list <- build_nested_list(split_files, list())
 
-# Function to generate HTML for a list
-generate_html <- function(x, name = NULL) {
-	if (is.list(x) && length(x) > 0) {
-		files_html <- map_chr(x[!sapply(x, is.list)], ~glue("<li><a href='{.}.md'>{.}</a></li>"))
-		dirs_html <- imap_chr(x[sapply(x, is.list)], ~generate_html(.x, name = .y))
-		inner_html <- c(files_html, unlist(dirs_html))
-		inner_html <- paste(inner_html, collapse = "")
+# Function to generate Markdown for a list
+generate_markdown <- function(x, name = NULL, level = 0) {
+  if (is.list(x) && length(x) > 0) {
+    files_md <- map_chr(x[!sapply(x, is.list)], ~glue("{paste(rep('  ', level), collapse = '')}- [{.}](https://github.com/zekrom-vale/ZekDex/wiki/{.})"))
+    dirs_md <- imap_chr(x[sapply(x, is.list)], ~generate_markdown(.x, name = .y, level = level + 1))
+    inner_md <- c(files_md, unlist(dirs_md))
+    inner_md <- paste(inner_md, collapse = "\n")
 
-		if (!is.null(name)) {
-			return(glue("<li>{name}<ul>{inner_html}</ul></li>"))
-		} else {
-			return(glue("<ul>{inner_html}</ul>"))
-		}
-	} else {
-		return(glue("<li><a href='{x}.md'>{x}</a></li>"))
-	}
+    if (!is.null(name)) {
+      return(glue("{paste(rep('  ', level-1), collapse = '')}- {name}\n{inner_md}"))
+    } else {
+      return(inner_md)
+    }
+  } else {
+    return(glue("{paste(rep('  ', level), collapse = '')}- [{x}](https://github.com/zekrom-vale/ZekDex/wiki/{x})"))
+  }
 }
 
-# Generate HTML for each file
-html <- generate_html(nested_list)
+# Generate Markdown for each file
+markdown <- generate_markdown(nested_list)
 
-# Combine all HTML into a single list
-glue("<ul>{html}</ul>")|>
-	write_file("wiki/_Sidebar.html")
+# Write the Markdown to the _Sidebar.md file
+write_file(markdown, "wiki/_Sidebar.md")
